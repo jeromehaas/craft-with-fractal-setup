ee74d9374739b98fa0d5709fdb0e23dc3354c97a
Jerome Haas <hello@jeromehaas.dev>
2022-03-18 17:46:25 +0100

    Refactor: comments in fractal.config.js

--- fractal/fractal.config.js
+++ fractal/fractal.config.js
@@ -1,29 +1,21 @@
 'use strict';
 
-const twig = require('@frctl/twig');
-
-/* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = module.exports = require('@frctl/fractal').create();
 
-/* Set the title of the project */
+/* SET TITLES */
 fractal.set('project.title', 'Fractal | component libary');
 fractal.set('project.version', 'v1.0');
 fractal.set('project.author', 'jeromehaas');
 
-/* Set the engines */
-// const twigAdapter = require('@frctl/twig')();
-// // fractal.docs.engine(twigAdapter);
-// // fractal.components.engine(twigAdapter);
-
-/* Tell Fractal where the components will live */
+/* DEFINE COMPONENTS DIRECTORY */
 fractal.components.set('path', __dirname + '/components');
 fractal.components.set('ext', '.hbs');
 
-/* Tell Fractal where assets will live */
+/* DEFINE ASSETS DIRECTORY */
 fractal.web.set('static.path', __dirname + '/public');
 
-/* Tell Fractal where the build folder is */
+/* DEFINE BUILD DIRECTORY */
 fractal.web.set('builder.dest', __dirname + '/build');
 
-/* Tell Fractal wich the default preview layout is */
+/* DEFINE PREVIEW LAYOUT */
 fractal.components.set('default.preview', '@preview');
